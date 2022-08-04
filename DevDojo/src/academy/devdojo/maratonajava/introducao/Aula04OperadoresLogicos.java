01389c10c90fafda65a834f1ef643f60c3946836
adrierjs <adrierj@gmail.com>
2022-08-03 22:10:29 -0300

    OperadoresLogicos

---
+++ DevDojo/src/academy/devdojo/maratonajava/introducao/Aula04OperadoresLogicos.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04OperadoresLogicos {
+    public static void main(String[] args) {
+        /*and(&&) or(||) */
+        int idade = 10;
+        if (idade<=10 && idade >=0){
+            System.out.println("É menor ou igual a 10 e maior ou igual a zero");
+
+        }
+    }
+}
