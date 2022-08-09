7f7d5898048d5581d241777ca82c0541b40e7515
adrierjs <adrierj@gmail.com>
2022-08-09 01:49:14 -0300

    Blocos de inicialização

--- DevDojo/src/academy/devdojo/maratonajava/javacore/Eblocosdeinicializacao/test/FilmesTest.java
+++ DevDojo/src/academy/devdojo/maratonajava/javacore/Eblocosdeinicializacao/test/FilmesTest.java
@@ -5,9 +5,7 @@ import academy.devdojo.maratonajava.javacore.Eblocosdeinicializacao.dominio.Film
 public class FilmesTest {
     public static void main(String[] args) {
         Filmes f1 = new Filmes();
-        for (int i = 0; i < f1.getEpisodios().length; i++) {
-            System.out.print(f1.getEpisodios()[i]+" ");
-        }
+
 
     }
 }
