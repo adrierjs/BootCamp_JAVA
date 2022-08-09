8eb15fd3f5aa026d9980cd44da4012dee7012087
adrierjs <adrierj@gmail.com>
2022-08-09 01:48:04 -0300

    Associação

---
+++ DevDojo/src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Carros.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Carros {
+    private String nome, marca;
+
+    public Carros() {
+    }
+
+    public Carros(String nome, String marca) {
+        this.nome = nome;
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
